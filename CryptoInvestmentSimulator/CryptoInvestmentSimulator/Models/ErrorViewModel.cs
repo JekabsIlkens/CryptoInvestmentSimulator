663f740a48a52958fc193281d8ae1dbcd82ca3ca
Jekabs Ilkens <jekabs4442@gmail.com>
2022-11-02 21:45:45 +0200

    v1 Added project template

---
+++ CryptoInvestmentSimulator/CryptoInvestmentSimulator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CryptoInvestmentSimulator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
