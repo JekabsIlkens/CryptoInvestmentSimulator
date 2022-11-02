21f48f39e3b30329b78c399b905bb24fe29fa3e0
Jekabs Ilkens <jekabs4442@gmail.com>
2022-11-02 21:57:29 +0200

    v1 Separated service registration from Porgram.cs

--- CryptoInvestmentSimulator/CryptoInvestmentSimulator/Program.cs
+++ CryptoInvestmentSimulator/CryptoInvestmentSimulator/Program.cs
@@ -1,7 +1,8 @@
-var builder = WebApplication.CreateBuilder(args);
+using CryptoInvestmentSimulator;
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.RegisterServices();
 
 var app = builder.Build();
 
@@ -9,19 +10,17 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
---
+++ CryptoInvestmentSimulator/CryptoInvestmentSimulator/ServiceConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace CryptoInvestmentSimulator
+{
+    public static class ServiceConfiguration
+    {
+        /// <summary>
+        /// Extension method that registers services for builder in Program.cs
+        /// </summary>
+        /// <param name="serviceCollection"></param>
+        /// <returns><see cref="IServiceCollection"/></returns>
+        public static IServiceCollection RegisterServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddControllersWithViews();
+            serviceCollection.AddRouting(options => options.LowercaseUrls = true);
+
+            return serviceCollection;
+        }
+    }
+}
