3becac38ea60839767221468d1f8c29caaf40897
Jekabs Ilkens <jekabs4442@gmail.com>
2022-11-05 20:23:21 +0200

    v2 Added portfolio page view and controller

---
+++ CryptoInvestmentSimulator/CryptoInvestmentSimulator/Controllers/PortfolioController.cs
@@ -0,0 +1,20 @@
+﻿using CryptoInvestmentSimulator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CryptoInvestmentSimulator.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
