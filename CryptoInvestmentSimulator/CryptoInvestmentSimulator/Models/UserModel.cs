cadbe55e35bc74c16c93702ff217227f79852360
Jekabs Ilkens <jekabs4442@gmail.com>
2022-11-06 01:10:54 +0200

    v3 Added user model and data display

---
+++ CryptoInvestmentSimulator/CryptoInvestmentSimulator/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿namespace CryptoInvestmentSimulator.Models
+{
+    public class UserModel
+    {
+        public int? UserId { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? EmailAddress { get; set; }
+
+        public string? AvatarUrl { get; set; }
+    }
+}
