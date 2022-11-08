1552d0c4786c7770499ae1551525abc8d2aa4715
Jekabs Ilkens <jekabs4442@gmail.com>
2022-11-08 21:12:48 +0200

    v4 Added tests for UserProcedures + Moq package

---
+++ CryptoInvestmentSimulator/UnitTests/DatabaseTests/DatabaseContextTests.cs
@@ -0,0 +1,36 @@
+﻿using CryptoInvestmentSimulator.Constants;
+using CryptoInvestmentSimulator.Database;
+using FluentAssertions;
+using Xunit;
+
+namespace UnitTests.DatabaseTests
+{
+    public class DatabaseContextTests
+    {
+        [Fact]
+        public void GetConnection_ConnectionStringValid_Success()
+        {
+            // Arrange
+            var databaseContext = new DatabaseContext(DatabaseConstants.Access);
+
+            // Act
+            var result = databaseContext.GetConnection();
+
+            // Assert
+            result.Should().NotBe(null);
+        }
+
+        [Fact]
+        public void GetConnection_ConnectionStringNull_Exception()
+        {
+            // Arrange
+            var databaseContext = new DatabaseContext("");
+
+            // Act
+            Action act = () => databaseContext.GetConnection();
+
+            // Assert
+            act.Should().Throw<ArgumentNullException>();
+        }
+    }
+}
