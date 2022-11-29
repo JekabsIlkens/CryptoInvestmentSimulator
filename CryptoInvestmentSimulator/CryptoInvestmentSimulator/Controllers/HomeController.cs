fde72a8776ffd3938f277f49117389159d1d124a
Jekabs Ilkens <jekabs4442@gmail.com>
2022-11-29 15:42:14 +0200

    v6 Home controller and views refactor

--- CryptoInvestmentSimulator/CryptoInvestmentSimulator/Controllers/HomeController.cs
+++ CryptoInvestmentSimulator/CryptoInvestmentSimulator/Controllers/HomeController.cs
@@ -10,33 +10,8 @@ namespace CryptoInvestmentSimulator.Controllers
 {
     public class HomeController : Controller
     {
-        [Authorize]
-        public IActionResult Index()
-        {
-            NewUserCheck();
-            EmailVerificationCheck();
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult SetUsername(string username)
-        {
-            var email = User.FindFirst(c => c.Type == ClaimTypes.Email)?.Value;
-            var context = new DatabaseContext(DatabaseConstants.Access);
-            var procedure = new UserProcedures(context);
-
-            procedure.UpdateUsername(email, username);
-            return View("Index");
-        }
-
-        public string GetUsername()
-        {
-            var email = User.FindFirst(c => c.Type == ClaimTypes.Email)?.Value;
-            var context = new DatabaseContext(DatabaseConstants.Access);
-            var procedure = new UserProcedures(context);
-            var user = procedure.GetUserDetails(email);
-            return user.Username;
-        }
+        private static readonly DatabaseContext context = new (DatabaseConstants.Access);
+        private static readonly UserProcedures procedures = new (context);
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
@@ -44,43 +19,87 @@ namespace CryptoInvestmentSimulator.Controllers
             return View(new ErrorModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        /// <summary>
+        /// Index view for home page.
+        /// Performs new user and email verification checks.
+        /// </summary>
+        /// <returns>Home view with user view model</returns>
+        [Authorize]
+        public IActionResult Index()
+        {
+            NewUserCheck();
+            EmailVerificationCheck();
+            var user = GetUserDetails();
+
+            return View(user);
+        }
+
+        /// <summary>
+        /// Performs username update after user fills in the new user popup.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns>Home view with user view model</returns>
+        [HttpPost]
+        public IActionResult SetUsername(string username)
+        {
+            var email = User.FindFirst(c => c.Type == ClaimTypes.Email)?.Value;
+            procedures.UpdateUsername(email, username);
+
+            var user = GetUserDetails();
+
+            return View("Index", user);
+        }
+
+        /// <summary>
+        /// Creates new user model with default username and verification status.
+        /// Performs new user insertion procedure.
+        /// </summary>
         private void NewUserCheck()
         {
             var user = new UserModel()
             {
-                Username = "TestName",
+                Username = "NewUser",
                 Email = User.FindFirst(c => c.Type == ClaimTypes.Email)?.Value,
                 AvatarUrl = User.FindFirst(c => c.Type == "picture")?.Value,
                 IsVerified = false
             };
 
-            var context = new DatabaseContext(DatabaseConstants.Access);
-            var procedure = new UserProcedures(context);
-
-            procedure.InsertNewUser(user);
+            procedures.InsertNewUser(user);
         }
 
+        /// <summary>
+        /// Compares Auth0 and database verification statuses.
+        /// If mismatch - performs verification status update procedure.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"></exception>
         private void EmailVerificationCheck()
         {
             var claims = User.Identities.First().Claims;
             var email = User.FindFirst(c => c.Type == ClaimTypes.Email)?.Value;
 
-            if (email == null || email == "")
+            if (string.IsNullOrEmpty(email))
             {
-                throw new ArgumentNullException(nameof(email));
+                throw new ArgumentNullException($"Received {nameof(email)} is null or empty!");
             }
 
             foreach (var claim in claims)
             {
                 if (claim.Type == "email_verified" && claim.Value == "true")
                 {
-                    var context = new DatabaseContext(DatabaseConstants.Access);
-                    var procedure = new UserProcedures(context);
-
-                    procedure.UpdateVerification(email);
+                    procedures.UpdateVerification(email);
                 }
 
             }
+        }
+
+        /// <summary>
+        /// Fills a user model for use in other methods.
+        /// </summary>
+        /// <returns>Filled user model</returns>
+        private UserModel GetUserDetails()
+        {
+            var email = User.FindFirst(c => c.Type == ClaimTypes.Email)?.Value;
+            return procedures.GetUserDetails(email);
         }
     }
 }
---
+++ CryptoInvestmentSimulator/CryptoInvestmentSimulator/wwwroot/js/home.js
@@ -0,0 +1,13 @@
+﻿
+// Functions to show/hide new user popup.
+function hideUsernameModal()
+{
+    $('#exampleModalCenter').modal('hide');
+}
+
+$(document).ready(function ()
+{
+    $('#exampleModalCenter').modal('show');
+
+    document.getElementById("saveUsername").addEventListener("click", hideUsernameModal);
+});
