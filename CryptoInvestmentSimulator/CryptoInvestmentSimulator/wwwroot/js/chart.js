de3dd9b6e00f3397bf44762f92c522df7a1d83af
Jekabs Ilkens <jekabs4442@gmail.com>
2022-12-03 15:15:23 +0200

    v7 Made chart rendering generic and moved it to separate js file

---
+++ CryptoInvestmentSimulator/CryptoInvestmentSimulator/wwwroot/js/chart.js
@@ -0,0 +1,49 @@
+﻿
+// Creates and configures a renderable price chart.
+function getChart(crypto, history)
+{
+	var chart = new CanvasJS.Chart("chartContainer",
+	{
+		zoomEnabled: false,
+		axisX:
+		{
+			title: "",
+			crosshair:
+			{
+				enabled: true,
+				snapToDataPoint: true
+			}
+		},
+		axisY:
+		{
+			crosshair:
+			{
+				enabled: true,
+				snapToDataPoint: true,
+				valueFormatString: "#,###0"
+			}
+		},
+		toolTip:
+		{
+			shared: true
+		},
+		legend:
+		{
+			dockInsidePlotArea: true,
+			verticalAlign: "top",
+			horizontalAlign: "right"
+		},
+		data:
+		[{
+			type: "line",
+			markerType: "none",
+			xValueType: "dateTime",
+			xValueFormatString: "hh:mm:ss",
+			name: crypto,
+			showInLegend: true,
+			dataPoints: history
+		}]
+	});
+
+	return chart;
+}
