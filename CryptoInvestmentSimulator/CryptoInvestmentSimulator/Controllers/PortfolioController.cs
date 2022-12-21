339a541d85db8b12c3b60efba48085c46721b7b5
Jekabs Ilkens <jekabs4442@gmail.com>
2022-12-22 01:48:56 +0200

    v9 Added wallet balances calculation to percentage split for correct wallet chart

--- CryptoInvestmentSimulator/CryptoInvestmentSimulator/Controllers/PortfolioController.cs
+++ CryptoInvestmentSimulator/CryptoInvestmentSimulator/Controllers/PortfolioController.cs
@@ -1,5 +1,6 @@
 ﻿using CryptoInvestmentSimulator.Constants;
 using CryptoInvestmentSimulator.Database;
+using CryptoInvestmentSimulator.Enums;
 using CryptoInvestmentSimulator.Helpers;
 using CryptoInvestmentSimulator.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
@@ -14,6 +15,7 @@ namespace CryptoInvestmentSimulator.Controllers
         private static readonly DatabaseContext context = new(DatabaseConstants.Access);
         private static readonly UserProcedures userProcedures = new(context);
         private static readonly WalletProcedures walletProcedures = new(context);
+        private static readonly MarketDataProcedures marketProcedures = new(context);
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
@@ -29,7 +31,7 @@ namespace CryptoInvestmentSimulator.Controllers
         public IActionResult Index()
         {
             var user = GetUserDetails();
-            ViewBag.WalletContents = walletProcedures.GetUsersWalletBalances(user.Id);
+            ViewBag.WalletPercent = GetWalletPercentageSplit(user.Id);
 
             return View("Index", user);
         }
@@ -62,6 +64,44 @@ namespace CryptoInvestmentSimulator.Controllers
         {
             var email = User.FindFirst(c => c.Type == ClaimTypes.Email)?.Value;
             return userProcedures.GetUserDetails(email);
+        }
+
+        /// <summary>
+        /// Converts wallet balances into a percent split of total value in wallets.
+        /// </summary>
+        /// <param name="userId">Wallet owner</param>
+        /// <returns>Wallet percentage split array</returns>
+        private decimal[] GetWalletPercentageSplit(int userId)
+        {
+            var walletBalances = walletProcedures.GetUsersWalletBalances(userId);
+            var btcLatest = marketProcedures.GetLatestMarketData(CryptoEnum.BTC);
+            var ethLatest = marketProcedures.GetLatestMarketData(CryptoEnum.ETH);
+            var adaLatest = marketProcedures.GetLatestMarketData(CryptoEnum.ADA);
+            var atomLatest = marketProcedures.GetLatestMarketData(CryptoEnum.ATOM);
+            var dogeLatest = marketProcedures.GetLatestMarketData(CryptoEnum.DOGE);
+
+            decimal[] allToEuro = new decimal[6];
+
+            allToEuro[0] = walletBalances.EuroAmount;
+            allToEuro[1] = btcLatest.UnitValue * walletBalances.BitcoinAmount;
+            allToEuro[2] = ethLatest.UnitValue * walletBalances.EtheriumAmount;
+            allToEuro[3] = adaLatest.UnitValue * walletBalances.CardanoAmount;
+            allToEuro[4] = atomLatest.UnitValue * walletBalances.CosmosAmount;
+            allToEuro[5] = dogeLatest.UnitValue * walletBalances.DogecoinAmount;
+
+            var hundredPercent = 0M;
+            for (int i = 0; i < allToEuro.Length; i++) hundredPercent += allToEuro[i];
+
+            decimal[] allToPercent = new decimal[6];
+
+            allToPercent[0] = (allToEuro[0] * 100) / hundredPercent;
+            allToPercent[1] = (allToEuro[1] * 100) / hundredPercent;
+            allToPercent[2] = (allToEuro[2] * 100) / hundredPercent;
+            allToPercent[3] = (allToEuro[3] * 100) / hundredPercent;
+            allToPercent[4] = (allToEuro[4] * 100) / hundredPercent;
+            allToPercent[5] = (allToEuro[5] * 100) / hundredPercent;
+
+            return allToPercent;
         }
     }
 }
